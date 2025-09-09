169a24f8ea090b3c74b73a5ee1c2e9d579f5f94e
장영철 <1231yw1103@naver.com>
2025-09-09 15:50:30 +0900

    1

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,18 @@
-// dashboard.js 파일에 아래 코드를 복사하세요.
+// dashboard.js 파일에 이 코드를 복사하세요.
 
-// 파이어베이스 데이터베이스 초기화
+// 파이어베이스 설정 코드
+const firebaseConfig = {
+  apiKey: "AIzaSyAC-6B0FV9UI9hFB_UzEl4iXVQDjq9ukV0",
+  authDomain: "yangonebinaievent.firebaseapp.com",
+  projectId: "yangonebinaievent",
+  storageBucket: "yangonebinaievent.firebasestorage.app",
+  messagingSenderId: "136715708338",
+  appId: "1:136715708338:web:cdf2743235b2829010a6ab",
+  measurementId: "G-CH73R4Y2M1"
+};
+
+// 파이어베이스 초기화
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const submissionsRef = db.collection('submissions');
 
@@ -13,24 +25,18 @@ submissionsRef.orderBy('date', 'desc').onSnapshot(snapshot => {
     submissionsTableBody.innerHTML = ''; // 기존 목록 초기화
     snapshot.forEach(doc => {
         const submission = doc.data();
-
-        // 새로운 행(<tr>) 생성
         const row = document.createElement('tr');
-
-        // 셀(<td>)에 데이터 넣기
         row.innerHTML = `
             <td>${doc.id}</td>
             <td>${submission.filename}</td>
             <td>${submission.date}</td>
             <td>${submission.name}</td>
         `;
-
-        // 테이블에 행 추가
         submissionsTableBody.appendChild(row);
     });
 });
 
 // 로그아웃 버튼 기능
 logoutBtn.addEventListener('click', () => {
-    window.location.href = 'admin.html'; // 로그인 페이지로 이동
+    window.location.href = 'admin.html';
 });
